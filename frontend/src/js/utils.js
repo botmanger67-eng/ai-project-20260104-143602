a99b0fc84c851cb8263e0f2e05221aab2829f68d
botmanger67-eng <botmanger67@gmail.com>
2026-01-04 19:36:24 +0500

    Initial commit: Auto-generated by Code Generator Bot

---
+++ frontend/src/js/utils.js
@@ -0,0 +1,19 @@
+// frontend/src/js/utils.js
+
+// Utility functions can be added here
+
+// Example: Format date
+function formatDate(date) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+    return new Date(date).toLocaleDateString(undefined, options);
+}
+
+// Example: Debounce function
+function debounce(func, delay) {
+    let timeout;
+    return function(...args) {
+        const context = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(context, args), delay);
+    };
+}
